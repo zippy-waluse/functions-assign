2d36f251ac6c8e8c07ecca9bbf59d45dc5c8b478
zippy-waluse <zippywaluse@gmail.com>
2024-04-03 12:28:00 +0300

    corrections

--- functions.js
+++ functions.js
@@ -1,34 +1,54 @@
 // Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
 
-function reverse(){
-let name = ['Hua','Dira','Jean']
-let secondname = name.sort() 
-console.log(name.sort());
-console.log(name.reverse())
+// function reverse(){
+// let name = ['Hua','Dira','Jean']
+// // let secondname = name.sort() 
+// console.log(name.sort());
+// console.log(name.reverse());
+// };
+
+// reverse()
+
+function reverseStrings(arr){
+
+  const reverseStrings =arr.map(str => [...str].reverse().join(''));
+  console.log(reverseStrings);
+  console.dm
 }
+reverseStrings(["Uganda","Tanzania","Malawi","Burundi","Burunfdi"]);
 
-reverse()
 
-
-// console.log(reverse(names));
 
 // Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
 
 
 
-        function checkElements() {
-            let arr =arr.forEach(element => {
-               if (element > 0) {
-                 console.log("positive");
-               } else if (element < 0) {
-                 console.log("negative");
-               } else {
-                 console.log("zero");
-               }
-            });
-           }
+  // function checkElements() {
+  //  let arr = []
+  // arr.forEach(element => {
+  //  if (element > 0) {
+  //  console.log("positive");
+  // } else if (element < 0) {
+  // console.log("negative");
+  // } else {
+  // console.log("zero");
+  //  }
+  // });
+  // }
     
+  // checkElements(1,2,3,4)
 
+  function checkElement(array){
+    array.forEach(element => {
+      if (element > 0) {
+        console.log(`${element} is positive`);
+      }else if(element < 0){
+        console.log(`${element} is negative`);
+      }else {
+        console.log(`${element} is zero`);
+      }
+    });
+  }
 
 
 // Given an array of objects, where each object represents an employee with an id, name, and salary property, write a function that returns a new array of employee objects sorted by their salary in ascending order.
@@ -47,10 +67,31 @@ function sortEmployer(employees) {
 // Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
 
 function multiply(numbers) {
-  number.forEach(function(number) {
-     console.log(number * 2);
+  numbers.forEach((number) => {
+    console.log(number * 2);
   });
  }
 
  const numbersArray = [8, 7, 2, 4, 5];
-   multiply(numbers);
+   multiply(numbersArray);
+
+
+   //Write a function
+
+  function transformArray(numbers){
+    numbers.forEach((number,index) => {
+     if (index < 4){
+        
+      number[index] = number * 8
+      
+     }else if (index >= number.length -2){
+
+      number[index] = number + 5;
+
+     }
+    })
+console.log(numbers);
+  }
+
+  const sort = [1,2,3,4,5];
+  numbersArray(sort);
